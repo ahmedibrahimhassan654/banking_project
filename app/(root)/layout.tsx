4ab83c43ec0c021f066f0f161a0c519d474c6b2c
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-08-08 22:44:57 +0300

    side bar implenting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNav from "@/components/ui/MobileNav";
+import Sidbar from "@/components/ui/Sidbar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const logedIn = { firstName: "Ahmed", lastName: "Hema" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidbar user={logedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={logedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
