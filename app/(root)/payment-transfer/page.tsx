b9139a10e1c73361638ee87b1c8aebcc11f6ffbe
Ahmed Ibrahim <ahmedibrahimhassan654@gmail.com>
2025-01-08 16:53:45 +0200

    finished auth form

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Transfer = () => {
+  return <div>payment-transfer</div>;
 };
 
-export default page;
+export default Transfer;
